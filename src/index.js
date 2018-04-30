7c0611773d58aeea2cdba37953d9afcb808bd728
Guillaume Zucchi <gzucchi.pro@gmail.com>
2018-04-30 09:41:46 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/app'
+
+const API_KEY = "5c84102fb17551ff3a5335d565983de9";
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
